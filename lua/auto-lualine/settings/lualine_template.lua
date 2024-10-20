bb2edc7e872a96693abc3819e69c75b2d7bfb65e
Pojok Code <asep.komarudin@dataon.com>
2024-10-20 23:02:06 +0700

    enc: add lsp progress config

--- lua/auto-lualine/settings/lualine_template.lua
+++ lua/auto-lualine/settings/lualine_template.lua
@@ -1,5 +1,25 @@
 local M = {}
 
+local function ambilKataDariKiri(kalimat, jumlahKata)
+  local hasil = ""
+  local posisiAwal = 1
+  local kataKe = 0
+
+  while kataKe < jumlahKata do
+    local posisiSpasi = string.find(kalimat, " ", posisiAwal)
+    if posisiSpasi then
+      hasil = hasil .. string.sub(kalimat, posisiAwal, posisiSpasi - 1) .. " "
+      posisiAwal = posisiSpasi + 1
+    else
+      hasil = hasil .. string.sub(kalimat, posisiAwal)
+      break
+    end
+    kataKe = kataKe + 1
+  end
+
+  return hasil
+end
+
 local component = require("auto-lualine.settings.lualine_component")
 local treesitter = component.treesitter
 local lsp_info = component.lsp_info
@@ -10,6 +30,19 @@ local get_branch = component.get_branch
 local lsp_progress = {}
 local data_ok, lspprogress = pcall(require, "lsp-progress")
 if data_ok then
+  lspprogress.setup({
+    client_format = function(client_name, spinner, series_messages)
+      return #series_messages > 0
+          and (spinner .. " " .. ambilKataDariKiri(table.concat(series_messages, ", "), 4) .. "...")
+        or nil
+    end,
+    format = function(client_messages)
+      local sign = ""
+      return #client_messages > 0
+          and (sign .. " " .. ambilKataDariKiri(table.concat(client_messages, " "), 4) .. "...")
+        or sign
+    end,
+  })
   lsp_progress = lspprogress.progress
 end
 local codeium = component.codeium
