3c16bbbfb300908bf467a2379427d4e22235f456
Pojok Code <asep.komarudin@dataon.com>
2024-10-20 23:12:58 +0700

    enc: add config lsp progress

--- lua/auto-lualine/init.lua
+++ lua/auto-lualine/init.lua
@@ -8,7 +8,45 @@ _G.switch = function(param, case_table)
   return def and def() or nil
 end
 
+local function ambilKataDariKiri(kalimat, jumlahKata)
+  local hasil = ""
+  local posisiAwal = 1
+  local kataKe = 0
+
+  while kataKe < jumlahKata do
+    local posisiSpasi = string.find(kalimat, " ", posisiAwal)
+    if posisiSpasi then
+      hasil = hasil .. string.sub(kalimat, posisiAwal, posisiSpasi - 1) .. " "
+      posisiAwal = posisiSpasi + 1
+    else
+      hasil = hasil .. string.sub(kalimat, posisiAwal)
+      break
+    end
+    kataKe = kataKe + 1
+  end
+
+  return hasil
+end
+
+
 M.setup = function(opts)
+  -- definisikan lsp progress
+  local data_ok, lspprogress = pcall(require, "lsp-progress")
+  if data_ok then
+    lspprogress.setup({
+      client_format = function(client_name, spinner, series_messages)
+        return #series_messages > 0
+            and (spinner .. " " .. ambilKataDariKiri(table.concat(series_messages, ", "), 4) .. "...")
+          or nil
+      end,
+      format = function(client_messages)
+        local sign = ""
+        return #client_messages > 0
+            and (sign .. " " .. ambilKataDariKiri(table.concat(client_messages, " "), 4) .. "...")
+          or sign
+      end,
+    })
+  end
   -- check config for theme
   local component = require("auto-lualine.settings.lualine_component")
   local colors = component.colors
